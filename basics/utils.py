cbd00f45accfebe942346199cdb74f7df5ffbd9d
Princekrampah <jsksprince@gmail.com>
2024-05-12 10:13:55 +0300

    Basics done

--- basics/utils.py
+++ basics/utils.py
@@ -8,28 +8,31 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import SimpleDirectoryReader
 
+from typing import Tuple
+
+
 async def create_router_query_engine(
     document_fp: str,
     verbose: bool = True,
 ) -> RouterQueryEngine:
     # load lora_paper.pdf documents
     documents = SimpleDirectoryReader(input_files=[document_fp]).load_data()
-    
+
     # chunk_size of 1024 is a good default value
     splitter = SentenceSplitter(chunk_size=1024)
     # Create nodes from documents
     nodes = splitter.get_nodes_from_documents(documents)
-    
+
     # LLM model
     Settings.llm = OpenAI(model="gpt-3.5-turbo")
     # embedding model
     Settings.embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
-    
+
     # summary index
     summary_index = SummaryIndex(nodes)
     # vector store index
     vector_index = VectorStoreIndex(nodes)
-    
+
     # summary query engine
     summary_query_engine = summary_index.as_query_engine(
         response_mode="tree_summarize",
@@ -38,7 +41,7 @@ async def create_router_query_engine(
 
     # vector query engine
     vector_query_engine = vector_index.as_query_engine()
-    
+
     summary_tool = QueryEngineTool.from_defaults(
         query_engine=summary_query_engine,
         description=(
@@ -52,8 +55,7 @@ async def create_router_query_engine(
             "Useful for retrieving specific context from the the Lora paper."
         ),
     )
-    
-    
+
     query_engine = RouterQueryEngine(
         selector=LLMSingleSelector.from_defaults(),
         query_engine_tools=[
@@ -62,6 +64,56 @@ async def create_router_query_engine(
         ],
         verbose=verbose
     )
-    
-    
+
     return query_engine
+
+
+async def create_doc_tools(
+    document_fp: str,
+    doc_name: str,
+    verbose: bool = True,
+) -> Tuple[QueryEngineTool, QueryEngineTool]:
+    # load lora_paper.pdf documents
+    documents = SimpleDirectoryReader(input_files=[document_fp]).load_data()
+
+    # chunk_size of 1024 is a good default value
+    splitter = SentenceSplitter(chunk_size=1024)
+    # Create nodes from documents
+    nodes = splitter.get_nodes_from_documents(documents)
+
+    # LLM model
+    Settings.llm = OpenAI(model="gpt-3.5-turbo")
+    # embedding model
+    Settings.embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
+
+    # summary index
+    summary_index = SummaryIndex(nodes)
+    # vector store index
+    vector_index = VectorStoreIndex(nodes)
+
+    # summary query engine
+    summary_query_engine = summary_index.as_query_engine(
+        response_mode="tree_summarize",
+        use_async=True,
+    )
+
+    # vector query engine
+    vector_query_engine = vector_index.as_query_engine()
+
+    summary_tool = QueryEngineTool.from_defaults(
+        name=f"{doc_name}_summary_query_engine_tool",
+        query_engine=summary_query_engine,
+        description=(
+            f"Useful for summarization questions related to the {doc_name}."
+        ),
+    )
+
+    vector_tool = QueryEngineTool.from_defaults(
+        name=f"{doc_name}_vector_query_engine_tool",
+        query_engine=vector_query_engine,
+        description=(
+            f"Useful for retrieving specific context from the the {doc_name}."
+        ),
+    )
+
+    return vector_tool, summary_tool
